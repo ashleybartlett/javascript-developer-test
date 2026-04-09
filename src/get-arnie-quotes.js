7eb66a1b01f0a7c15c33f8a4bac2c87b44d53b6c
Ashley Bartlett <ashley.bartlett@gmail.com>
2026-04-09 22:20:57 +1000

    Solved developer test

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,35 @@
 const { httpGet } = require('./mock-http-interface');
 
+const getArnieQuote = async (url) => {
+  const response = await httpGet(url)
+  const responseBody = JSON.parse(response.body)
+
+  if (!("message" in responseBody)) {
+    throw new Error(`Missing 'message' field in response body call to ${url}`)
+  }
+
+  const { message } = responseBody;
+
+  if (response.status !== 200) {
+    return {"FAILURE": message}
+  } else {
+    return {"Arnie Quote": message}
+  }
+}
+
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const queries = await Promise.allSettled(
+    urls.map(u => getArnieQuote(u))
+  );
+
+  const results = queries.map(q => {
+    if(q.status !== "fulfilled") {
+      throw q.reason
+    } 
+    return q.value
+  })
+
+  return results;
 };
 
 module.exports = {
